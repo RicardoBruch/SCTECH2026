a286a7e3024ca30d9b5e403fb79b61b9d9692a7f
Ricardo Bruch <112019920+RicardoBruch@users.noreply.github.com>
2026-07-26 21:52:17 -0300

    Enviando o restante dos arquivos de outros exercicios

---
+++ Exercicios/listaDeNomes/Program.cs
@@ -0,0 +1,24 @@
+﻿/// Exercicio de agora receber nomes e preencher na lista ate que o usuario digite sair, quando digitar sair deve mostrar todos os nomes que foram preenchidos
+
+List<string> nomes = new List<string>();
+
+while (true)        
+{
+    Console.WriteLine("Digite um nome ou 'sair' para encerrar o programa:");
+    string nome = Console.ReadLine();
+
+    if (nome == "sair")
+    {
+        break;
+    }
+
+    nomes.Add(nome);
+}
+
+Console.WriteLine("Nomes digitados:");
+
+foreach (string nome in nomes)
+{
+    Console.WriteLine("- " + nome);
+}
+
---
+++ Exercicios/repeticaoFatorial/Program.cs
@@ -0,0 +1,39 @@
+﻿
+Console.WriteLine("Digite um número para calcular o fatorial: ");
+int numero = int.Parse(Console.ReadLine());
+
+int fatorial = 1;
+for (int i = 1; i <= numero; i++)
+{
+    fatorial *= i;
+}
+Console.WriteLine($"O fatorial de {numero} é {fatorial}");
+
+// Console.WriteLine("Digite um número para calcular o fatorial: ");
+// int numero = int.Parse(Console.ReadLine());
+
+// int fatorial = 1;
+// int i = 1;
+
+// while (i <= numero)
+// {
+//     fatorial *= i;
+//     i++;
+// }
+
+// Console.WriteLine($"O fatorial de {numero} é {fatorial}");
+
+// Console.WriteLine("Digite um número para calcular o fatorial: ");
+// int numero = int.Parse(Console.ReadLine());
+
+// int fatorial = 1;
+// int i = 1;
+
+// do
+// {
+//     fatorial *= i;
+//     i++;
+// } while (i <= numero);
+
+// Console.WriteLine($"O fatorial de {numero} é {fatorial}");
+
